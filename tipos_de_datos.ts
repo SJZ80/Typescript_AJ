0676475b6f6e1cb5478d99eacef41cbd3a4510ef
SJZ80 <silvio.zabala@gmail.com>
2019-09-16 22:31:08 -0300

    Tipos de datos

---
+++ tipos_de_datos.ts
@@ -0,0 +1,12 @@
+//https://www.typescriptlang.org/docs/handbook/enums.html
+let buleano:boolean = true;
+let numerico:number = 12.9;
+let cadena:string = "cadena";
+let array: string[]= ["Silvio","Zabala"];
+let tupla:[string,string]=["Clave","valor"];
+enum colores {amarillo,azul,rojo};
+
+
+
+
+console.log(colores.azul + colores.rojo);
