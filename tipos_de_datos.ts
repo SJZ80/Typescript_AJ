8e255509654f8c0e3054aa19b3a7e647ad78a4de
Silvio Zabala <silvio.zabala@gmail.com>
2019-09-17 11:24:42 -0300

    Ejemplos con enumerados

--- tipos_de_datos.ts
+++ tipos_de_datos.ts
@@ -3,14 +3,38 @@ let buleano:boolean = true;
 let numerico:number = 12.9;
 let cadena:string = "cadena";
 let array: string[]= ["Silvio","Zabala"];
+
 let tupla:[string,string]=["Clave","valor"];
+
+//Enumerados
+enum deString {azul="jaja",rojo=10};
+
+let valorenum:deString = deString.rojo;
+let valorenum1:deString = deString.azul;
+console.log(typeof valorenum);
+console.log(typeof valorenum1);
+
+let valorTextualEnum:string = deString[1];
+
+console.log(`Valor Enumerado ${valorenum} Valor textual ${valorTextualEnum}`);
+
 enum colores {amarillo,azul,rojo};
+let valorEnumerado:colores = colores.rojo;
+let valorEnumerado1:number = colores.rojo;
+let valorTextualEnumerado:string = colores[2];
+
+console.log(`Valor Enumerado ${valorEnumerado} Valor textual ${valorTextualEnumerado}`);
+
+
+//Fin de Enumerados
+
 let cualquiera:any = {
     nombre:'Silvio',
     edad: 39
 };
 
-console.log(cualquiera);
+
+
 
 
 
